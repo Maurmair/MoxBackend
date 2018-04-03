8cee103349e86b0cbe7e5848ef08fb01708a4fcf
Maurmair <maurmair@gmail.com>
2018-04-03 15:41:09 +0200

    Basic CRUD Operations on InputData model

--- MoxBackend/MoxBackend/Controllers/InputDataController.cs
+++ MoxBackend/MoxBackend/Controllers/InputDataController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MoxBackend.Models;
+using MoxBackend.Persistence;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,30 +13,65 @@ namespace MoxBackend.Controllers
     public class InputDataController : ApiController
     {
         // GET: api/InputData
-        public IEnumerable<string> Get()
+        public ArrayList Get()
         {
-            return new string[] { "value1", "value2" };
+            InputDataPersistence ip = new InputDataPersistence();
+            return ip.getAllInputData();
         }
 
         // GET: api/InputData/5
-        public string Get(int id)
+        public InputData Get(DateTime Id)
         {
-            return "value";
+            InputDataPersistence ip = new InputDataPersistence();
+            InputData inputData = ip.getInputData(Id);
+            return inputData;
         }
 
         // POST: api/InputData
-        public void Post([FromBody]string value)
+        public HttpResponseMessage Post([FromBody]InputData inputDataValue)
         {
+            InputDataPersistence ip = new InputDataPersistence();
+            String date;
+            date = ip.saveInputData(inputDataValue);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+            response.Headers.Location = new Uri(Request.RequestUri, String.Format("/{0}", date));
+            return response;
         }
 
         // PUT: api/InputData/5
-        public void Put(int id, [FromBody]string value)
+        public HttpResponseMessage Put(DateTime Id, [FromBody]InputData inputDataValue)
         {
+            InputDataPersistence ip = new InputDataPersistence();
+            bool recordExisted = false;
+            recordExisted = ip.updateInputData(Id, inputDataValue);
+            HttpResponseMessage response;
+            if (recordExisted)
+            {
+                response = Request.CreateResponse(HttpStatusCode.NoContent);
+            }
+            else
+            {
+                response = Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            return response;
         }
 
         // DELETE: api/InputData/5
-        public void Delete(int id)
+        public HttpResponseMessage Delete(DateTime Id)
         {
+            InputDataPersistence ip = new InputDataPersistence();
+            bool recordExisted = false;
+            recordExisted = ip.deleteInputData(Id);
+            HttpResponseMessage response;
+            if (recordExisted)
+            {
+                response = Request.CreateResponse(HttpStatusCode.NoContent);
+            }
+            else
+            {
+                response = Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            return response;
         }
     }
 }
--- MoxBackend/MoxBackend/Persistence/InputDataPersistence.cs
+++ MoxBackend/MoxBackend/Persistence/InputDataPersistence.cs
@@ -2,10 +2,127 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MySql.Data;
+using MoxBackend.Models;
+using System.Collections;
 
 namespace MoxBackend.Persistence
 {
     public class InputDataPersistence
     {
+        private MySql.Data.MySqlClient.MySqlConnection conn;
+        public InputDataPersistence()
+        {
+            string myConnectionString = "server=108.167.172.114;uid=maurmair_moxAdm;pwd=RandomPw!;database=maurmair_moxDb";
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                ex.ToString();
+            }
+        }
+
+        public String saveInputData(InputData inputDataToSave)
+        {
+            string sqlString = "INSERT INTO InputData (Date, ActiveMinutesReached, StepsReached) VALUES ('"
+                + inputDataToSave.Date.ToString("yyyy-MM-dd") + "',"
+                + inputDataToSave.ActiveMinutesReached + ","
+                + inputDataToSave.StepsReached + ")";
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+            cmd.ExecuteNonQuery();
+            String id = cmd.LastInsertedId.ToString();
+            return id;
+        }
+
+        public InputData getInputData(DateTime Id)
+        {
+            InputData inputData = new InputData();
+            MySql.Data.MySqlClient.MySqlDataReader mySqlReader = null;
+            String sqlString = "SELECT * FROM InputDate WHERE Date = '" + Id.ToString("yyyy-MM-dd") + "'";
+
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+            mySqlReader = cmd.ExecuteReader();
+
+            if (mySqlReader.Read())
+            {
+                inputData.Date = mySqlReader.GetDateTime(0);
+                inputData.ActiveMinutesReached = mySqlReader.GetInt32(1);
+                inputData.StepsReached = mySqlReader.GetInt32(2);
+                return inputData;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public ArrayList getAllInputData()
+        {
+            ArrayList inputDataArray = new ArrayList();
+            MySql.Data.MySqlClient.MySqlDataReader mySqlReader = null;
+            String sqlString = "SELECT * FROM InputData";
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+            mySqlReader = cmd.ExecuteReader();
+            while (mySqlReader.Read())
+            {
+                InputData inputData = new InputData();
+                inputData.Date = mySqlReader.GetDateTime(0);
+                inputData.ActiveMinutesReached = mySqlReader.GetInt32(1);
+                inputData.StepsReached = mySqlReader.GetInt32(2);
+                inputDataArray.Add(inputData);
+            }
+            return inputDataArray;
+        }
+
+        public bool deleteInputData(DateTime Id)
+        {
+            InputData inputData = new InputData();
+            MySql.Data.MySqlClient.MySqlDataReader mySqlReader = null;
+
+            String sqlString = "SELECT * FROM InputData WHERE Date = '" + Id.ToString("yyyy-MM-dd") + "'";
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+            mySqlReader = cmd.ExecuteReader();
+            if (mySqlReader.Read())
+            {
+                mySqlReader.Close();
+                sqlString = "DELETE FROM InputData WHERE Date = '" + Id.ToString("yyyy-MM-dd") + "'";
+                cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool updateInputData(DateTime Id, InputData inputDataToSave)
+        {
+            MySql.Data.MySqlClient.MySqlDataReader mySqlReader = null;
+            String sqlString = "SELECT * FROM InputData WHERE Date = '" + Id.ToString("yyyy-MM-dd") + "'";
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+            mySqlReader = cmd.ExecuteReader();
+            if (mySqlReader.Read())
+            {
+                mySqlReader.Close();
+                sqlString = "UPDATE InputData Set Date='"
+                    + inputDataToSave.Date.ToString("yyyy-MM-dd") + "', ActiveMinutesReached= "
+                    + inputDataToSave.ActiveMinutesReached + ", StepsReached="
+                    + inputDataToSave.StepsReached + " WHERE Date = '"
+                    + Id.ToString("yyyy-MM-dd") + "'";
+                cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
